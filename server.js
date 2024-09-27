fb386aa68cb31409eae993229e041144a68f337e
Stephenolasunkanmi <olasunkanmistephen45@gmail.com>
2024-09-27 14:35:36 +0100

    temp

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 const app = express();
@@ -17,8 +18,15 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Routes
 app.use('/auth', require('./routes/auth'));
 app.use('/subscriptions', require('./routes/subscriptions'));
-app.use('/platforms', require('./routes/platform'))
+app.use('/platforms', require('./routes/platform'));
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all handler to return the React app for any routes not matched above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
